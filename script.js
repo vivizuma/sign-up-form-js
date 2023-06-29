70258f36abfe76bef9cf9cd2aaef6d1f10688645
Vito <vitomanz@hotmail.co.uk>
2023-06-29 12:46:29 +0100

    uhh

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ const APP = {
 
   InitListeners() {
     //DOM Elements
+    const form = document.getElementById("fakeForm");
     const emailInput = document.getElementById("email");
     const postcodeInput = document.getElementById("postcode");
     const passwordInput = document.getElementById("password");
     const passwordConfirmInput = document.getElementById("pass-confirm");
     //Event Listeners
-    emailInput.addEventListener("click", () => {
-      console.log("bruh");
-    });
+    //after entire value is changed
+    emailInput.addEventListener("change", APP.testEmail)
+ 
+
+    //live while typing
+    postcodeInput.addEventListener("input", () => console.log("broseph"));
+
+    //when form is submitted
+    form.addEventListener("submit", console.log("checking"));
   },
   testEmail(ev) {
-    console.log("hello test email");
     let email = ev.target;
+    console.log(email);
   },
-  //event listeners
+  //Validity Tests
+  testPostcode(ev) {},
 };
 
 document.addEventListener("DOMContentLoaded", APP.init);
